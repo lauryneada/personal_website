e367b2b243c7746be53ac46417e457977f5350c1
lauryne <lauryne.assa@gmail.com>
2023-08-26 18:04:50 -0400

    project fixes + contact + footer

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Col from 'react-bootstrap/Col';
 function Nav() {
   return (
     <Container>
-      <Row>
+      <Row className="sectionheader">
         <Col className="nav-item">about</Col>
         <Col className="nav-item">experience</Col>
         <Col className="nav-item">projects</Col>
@@ -63,33 +63,36 @@ function Experience() {
   );
 }
 
-function Card({project}) {
+function Card({project, children}) {
+  let apps = project.apps;
   return (
     <div className="card">
 
         <div classname="card-img">
-          <img src={project.img}></img>
+          <img className="desc" src={project.img}></img>
         </div>
         
         <div className="card-content">
           <h3>{project.title}</h3>
-
-          <p className="tech">{project.technologies.join(" ")}</p>
-
+          <p className="tech">{project.technologies.join(" - ")}</p>
           <p className="body">{project.body}</p>
         </div>
 
-        <div className="cardfooter">
-          <Row>
-            <Col>
-              <img src="github.svg"></img>
-            </Col>
-            <Col>
-              <img src="link.svg"></img>
-            </Col>
-          </Row>
-        </div>  
+        {children}
     </div>
+  );
+}
+
+function CardFooter({apps}) {
+  return (
+    <div className="cardfooter">
+      <Row className="row-cardfooter">
+        {apps.github && (<Col md="auto"> <img src="github.svg"></img> </Col>)}
+        {apps.chrome && (<Col md="auto"> <img src="chrome.svg"></img> </Col>)}
+        {apps.tableau && (<Col md="auto"> <img src="graph.svg"></img> </Col>)}
+        {apps.link && (<Col md="auto"> <img src="link.svg"></img> </Col>)}
+      </Row>
+    </div>  
   );
 }
 
@@ -98,18 +101,75 @@ function Projects() {
     <div className="section">
       <h2>Projects</h2>
       <Row>
-        <Col>
+        <Col className="col-card">
           <Card project = {{ img : "/portfolio.png",
                              title: "Portfolio Website", 
                              technologies: ["React", "Javascript", "HTML", "SCSS"],
                              body : "The website you're currently on! This is a project I worked on to practice using React which I recently" 
                                   + "started learning and plan to update as I learn more",
-                          }}/>
+                          }}>
+
+            <CardFooter apps={{github: true, link: true}}/>
+
+          </Card>
+        </Col>
+
+        <Col className="col-card">
+          <Card project = {{ img : "/studyspace.png",
+                             title: "StudySpace", 
+                             technologies: ["Javascript", "HTML", "SCSS"],
+                             body : "StudySpace is a productivity chrome extension that won 2nd place at the Queen's Programming Challenge in 2021",
+                          }}>
+            
+            <CardFooter apps={{github: true, chrome: true, link:true}}/>
+          
+          </Card>
+        </Col>
+
+        <Col className="col-card">
+          <Card project = {{ img : "/datascience.png",
+                             title: "Data Analysis Project", 
+                             technologies: ["Python", "Pandas", "Seaborn", "Matplotlib"],
+                             body : "As part of DS4A, I contributed to a data analysis of the impact COVID-19 had on food insecurity." +
+                                    "The project includes a report, datafolio, tableau dashboard and presentation all available on Github.",
+                          }}>
+
+            <CardFooter apps={{github: true, tableau: true}}/>
+
+          </Card>
         </Col>
 
       </Row>
     </div>
   );
+}
+
+function Contact() {
+  return(
+    <div className="sectioncontact">
+      <h2>Contact Me</h2>
+      <Row className="row-contact">
+        <Col>
+          <img className="contact-logo" src="/linkedin.png"></img>
+        </Col>
+        <Col>
+          <img className="contact-logo" src="/github-logo.png"></img>
+        </Col>
+        <Col>
+          <img className="contact-logo" src="/gmail.png"></img>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+function Footer() {
+  return(
+    <div className="sectionfooter">
+      <p className="endtext-top">Built & designed by Lauryne 👩🏾‍💻</p>
+      <p className="endtext">© 2023 Lauryne Assa</p>
+    </div>
+  )
 }
 
 export default function Main() {
@@ -119,6 +179,8 @@ export default function Main() {
       <Header />
       <About />
       <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
